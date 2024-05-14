452aa5a4c5a534313657bcde8ef3a0946a594aa8
Shaw Islam <shawnislam.ice@gmail.com>
2024-05-14 13:06:27 +0600

    review testimonial added

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -2,10 +2,16 @@ import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../hooks/useAxiosSecure";
 import ReviewCard from "./ReviewCard";
 import { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
 
 const Reviews = () => {
   const axiosSecure = useAxiosSecure();
-  const [sort,setSort]=useState('dsc')
+  const [sort, setSort] = useState("dsc");
+  
   const {
     data: reviews = [],
     error,
@@ -23,23 +29,39 @@ const Reviews = () => {
   refetch();
   return (
     <div className="my-5 md:my-10 container mx-auto max-w-screen-xl">
-      <div className="">
-        <hr className="my-3 border-gray-200  dark:border-gray-700" />
-        <h2 className="text-center text-3xl font-semibold">Reviews</h2>
-        <p className="max-w-xl mx-auto opacity-90 text-center pt-2">
-          See What Our Happy Customer Says! The review section on our platform
-          serves as a valuable resource for users to share their experiences and
-          insights on various products, services, or experiences. It provides a
-          platform for customers to express their opinions, which can be
-          instrumental for others making informed decisions.
-        </p>
-        <hr className="my-3 border-gray-200  dark:border-gray-700" />
-      </div>
-      <div className="grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-3 md:max-w-6xl mx-auto md:gap-5 place-items-center">
+      <section className="bg-white dark:bg-gray-900">
+        <div className="container px-6 py-10 mx-auto">
+          <div className="mt-6 md:flex md:items-center md:justify-between">
+            <div>
+              <h1 className="text-2xl font-semibold text-gray-800 capitalize lg:text-3xl dark:text-white">
+                What our clients are saying
+              </h1>
+
+              <div className="flex mx-auto mt-6">
+                <span className="inline-block w-40 h-1 bg-blue-500 rounded-full"></span>
+                <span className="inline-block w-3 h-1 mx-1 bg-blue-500 rounded-full"></span>
+                <span className="inline-block w-1 h-1 bg-blue-500 rounded-full"></span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination,Autoplay]}
+        autoplay={{ delay: 1000 }}
+        className="mySwiper"
+      >
         {reviews.map((review) => (
-          <ReviewCard key={review._id} review={review}></ReviewCard>
+          <SwiperSlide key={review._id}>
+            <ReviewCard review={review} />
+          </SwiperSlide>
         ))}
-      </div>
+      </Swiper>
     </div>
   );
 };
